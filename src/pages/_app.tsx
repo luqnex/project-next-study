bbb4bb34f6eddd4690d2c922a54dfc29cfa43a16
luqnex <lucas.costa4002@gmail.com>
2022-02-04 19:40:23 -0300

    feat: :sparkles: Add loading progress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,25 @@
 import type { AppProps } from "next/app";
 
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+import { ThemeProvider } from "@os-design/theming";
+import { initNProgress, NProgressGlobalStyles } from "@os-team/next-nprogress";
+
 import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
+	const router = useRouter();
+
+	useEffect(() => {
+		initNProgress(router);
+	}, [router]);
+
 	return (
-		<>
+		<ThemeProvider>
+			<NProgressGlobalStyles />
 			<Component {...pageProps} />
-		</>
+		</ThemeProvider>
 	);
 }
 
